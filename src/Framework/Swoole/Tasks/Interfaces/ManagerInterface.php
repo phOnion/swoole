94180908a7508c199c5e620032585473ba846397
Dimitar Dimitrov <daghostman.dd@gmail.com>
2019-01-06 23:34:53 +0200

    Add `all` & `race` methods for tasks

--- src/Framework/Swoole/Tasks/Interfaces/ManagerInterface.php
+++ src/Framework/Swoole/Tasks/Interfaces/ManagerInterface.php
@@ -2,7 +2,7 @@
 declare(strict_types=1);
 namespace Onion\Framework\Swoole\Tasks\Interfaces;
 
-use GuzzleHttp\Promise\Promise;
+use GuzzleHttp\Promise\PromiseInterface as Promise;
 use Onion\Framework\Swoole\Tasks\Task;
 
 interface ManagerInterface
@@ -10,6 +10,8 @@ interface ManagerInterface
     public function async(Task $task): Promise;
     public function sync(Task $task, int $timeout = 1): Promise;
     public function parallel(array $task, int $timeout = 1): Promise;
+    public function all(array $tasks, ?int $timeout = 10000): Promise;
+    public function race(array $tasks, ?int $timeout = 10000): Promise;
     public function schedule(Task $task, int $interval): Promise;
     public function delay(Task $task, int $interval): Promise;
 }
